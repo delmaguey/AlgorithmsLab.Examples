3ff12dc9398001baf999750d9fc7388a21286907
Del Maguey Technologies <dan13lpj@hotmail.com>
2026-04-11 23:51:33 -0600

    Update: Adding TwoSum algorithm for sorted array

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using AlgoritmosLab.Arrays;
+using Arrays;
 
 Console.WriteLine("--- Executing Algorithms ---");
 
 Console.WriteLine("");
 
 // Exercise: Disemvowel
-String txt = "This website is for losers LOL!";
-String res = Disem.DisemVowel(txt);Console.WriteLine($"The text without vowels is: {res}");
+// String txt = "This website is for losers LOL!";
+// String res = Disem.DisemVowel(txt);Console.WriteLine($"The text without vowels is: {res}");
 
+
+// Exercise: TwoSum
+int[] nums = [4,5,6];
+int target = 10;
+int[] res = TwoSum.Solve(nums, target);
+Console.WriteLine($"The indices of the two numbers that add up to {target} are: [{res[0]}, {res[1]}]");
