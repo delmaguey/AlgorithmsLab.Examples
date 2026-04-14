9ace331893f227b0d670edf28831386acf0eb36a
Del Maguey Technologies <dan13lpj@hotmail.com>
2026-04-13 19:59:20 -0600

    Update: Adding length validations and comments

--- src/Arrays/TwoSum.cs
+++ src/Arrays/TwoSum.cs
@@ -38,22 +38,33 @@ Only one valid answer exists.
 public static class TwoSum {
     public static int[] Solve(int[] nums, int target) {
 
-        int start =0;
+        if(nums.Length < 2 || nums.Length > 1000)
+        {
+            throw new ArgumentException("Array length is not valid. There should be at least 2 elements and at most 1000 elements.");
+        }
+
+        // Starting Two Pointers
+        int start = 0;
         int end = nums.Length-1;
 
-        while (start< end)
+        //Iterate until two pointers are different
+        while (start < end)
         {
+            // Sum the values of the pointers
             int temp = nums[start]+nums[end];
             if (temp == target)
             {
+                //If the sum is equal to target, return the indices ofthe pointers
                 return new int[]{start, end};
             }
             else if (temp > target)
             {
+                // Since the array is sorted, if the sum is greater than target, increment the pointer of end variable
                 end--;
             }
             else
             {
+                // if the sum is less than target, increment the pointer of start variable
                 start++;
             }
         }
