2e4e033bed76c872d5f93109a7294e0511dc8cea
Del Maguey Technologies <dan13lpj@hotmail.com>
2026-05-09 23:44:47 -0600

    Update: Optimizing longest palindrome in substring

--- src/Arrays/LongestPalindrome.cs
+++ src/Arrays/LongestPalindrome.cs
@@ -2,33 +2,49 @@ public static class LongestPalindrome
 {
     public static string Solve(string s)
     {
-        int n = s.Length;
-        string longest = "";
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                string substring = s.Substring(i, j - i + 1);
-                
-                if (IsPalindrome(substring) && substring.Length > longest.Length) {
-                    longest = substring;
-                }
+        int start = 0;
+        int maxLength = 0;
+
+        // Iterate through each character in the string
+        for (int i = 0;i<s.Length;i++)
+        {
+            // Expand window for odd and even lenghts
+            int[] odd  = ExpandWindow(s, i, i);
+            int[] even = ExpandWindow(s, i, i + 1);
+
+            // check odd
+            int oddLen = odd[1] - odd[0] + 1;
+            if (oddLen > maxLength) {
+                maxLength = oddLen;
+                start = odd[0];
             }
+
+            // check even
+            int evenLen = even[1] - even[0] + 1;
+            if (evenLen > maxLength) {
+                maxLength = evenLen;
+                start = even[0];
+            }
+
         }
-        
-        return longest;
+
+        return s.Substring(start, maxLength);
+
     }
 
-    private static bool IsPalindrome(string str) {
-        int left = 0;
-        int right = str.Length - 1;
-        
-        while (left < right) {
-            if (str[left] != str[right]) {
-                return false;
-            }
-            left++;
-            right--;
+    // expand the window until the characters in both sides are the same
+    private static int[] ExpandWindow(string s, int left, int right) {
+            
+        while(left >=0 && right < s.Length && s[left] == s[right])
+        {
+            left--;
+            right++;
         }
-        
-        return true;
+
+        return new int[] { left + 1, right - 1 };
+
     }
+
+
+    
 }
